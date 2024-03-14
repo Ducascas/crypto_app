06213bd22429d01e50a303690cf9f20ac45a8a34
Klimov Vadim <vp05178@yandex.ru>
2024-03-15 00:26:52 +0400

    commit

--- lib/features/crypto_coins/presentation/widgets/coin_card.dart
+++ lib/features/crypto_coins/presentation/widgets/coin_card.dart
@@ -1,6 +1,6 @@
 import 'package:chart_sparkline/chart_sparkline.dart';
-import 'package:crypto_app/features/crypto_coins/crypto_coins.dart';
 import 'package:flutter/material.dart';
+import 'package:crypto_app/features/crypto_coins/crypto_coins.dart';
 
 class CoinCard extends StatelessWidget {
   const CoinCard({
